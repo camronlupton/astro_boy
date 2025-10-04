adfd9bb7dd34033f000ed91fc10ecd1face34c2d
camronlupton <camron@qrewtech.com>
2025-10-04 11:59:29 -0500

    printing game

--- astro_boy_game/main.py
+++ astro_boy_game/main.py
@@ -5,6 +5,20 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    gameTick = 1
+    while gameTick >= 0 :
+        gameTick +=1
+        ## allows user to close window (copy pasted from course)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+        ##print(gameTick)
+
+        screen.fill((0, 0, 0))
 
 
 if __name__ == "__main__":
